04e5811aa4eb1d9884466d9c0044468280c1c8f4
juanjose <juanjoperez2121@gmail.com>
2024-07-19 11:01:24 -0500

    Se crea la estrucutra y las rutas del jsx, tambien  se crean las carpetas con sus componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './App.css';
+import Home from  '.';
+import ClickCounter from  '.';
+import Timer from  '.';
+import UserActive from  '.';
+import DigitalClock from '.';
 
-
-export default function Home() { 
-    const [valor, setValor] = useState(0)
-
-
-   const sumar =  () =>  {
-    const nuevoValor = valor + 1 
-        setValor(nuevoValor );
-        actualizarValor(nuevoValor);
-    };
-
-    const restar =  () =>  {
-        const nuevoValor = valor - 1 
-        setValor(nuevoValor);
-        actualizarValor(nuevoValor);
-    };
 
    
-
+const App = () => {
   return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/counter">Contador</Link>{" "}
+          <Link to="/timer">Cronometro</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/clock">Reloj</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/timer" element={<Timer />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/clock" element={<DigitalClock />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-   <>
-      <div className='container-all'>
-        <div className='container-suma' >
-          <p>Numero de clics:</p>
-            <span id="valor">{valor} </span>
-        </div>
-
-          
-        <button onClick={sumar}>Haz click</button>
-    </div>
-   </>
-  )
-}
+export default App;
